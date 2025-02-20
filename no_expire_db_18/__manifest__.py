b710a987be3b9f27b8cbd5ffef57a32b71c1f42e
Muhammad Ahsan Ismail <ahsan.ismail0159@gmail.com>
2025-02-20 11:21:23 +0500

    Add files via upload

---
+++ no_expire_db_18/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "No Expire DB 18",
+
+    'summary': "No Expire DB 18",
+
+    'description': """
+No Expire DB 18
+    """,
+
+    'author': "M.Rizwan",
+    'website': "https://www.tech-toch.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'mail'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+    ],
+}
+

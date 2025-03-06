9929813dc642943930b6c5867777198daf76924d
Muhammad Ahsan Ismail <ahsan.ismail0159@gmail.com>
2025-03-06 10:37:39 +0500

    Add files via upload

---
+++ cr_backdate_entries/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Backdate Entries',
+    'version': '18.0',
+    'category': 'Extra Tools',
+    'author': "Ahsan",
+    'website': "suitepark.com",
+    'summary': 'The Backdate Entries module allows users to enter transactions on a date prior to the current date.',
+    'depends': [
+        'stock','account','sale','purchase', 'mrp','stock', 'stock_account'
+    ],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'wizard/backdate_wiz_view.xml',
+        'views/backdate_action.xml'
+    ],
+    "license": "AGPL-3",
+    'images': ['static/description/banner.gif'],
+    "auto_install": False,
+    "installable": True,
+    "application": True,
+}
